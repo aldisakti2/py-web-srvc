3f3ebe1d54505f87c628e8216b71a645f78171e9
Qwwn <92309945+Qwwn@users.noreply.github.com>
2023-01-19 22:54:32 +0700

    update
    add sidebar dan lain lain

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import base64
+import streamlit as st #lib to build web app
+import pandas as pd #lib to handle dataframe and webscrapping
+import matplotlib.pyplot as plt #to create plot
+import plotly.express as px
+import base64   #to handle data download (csv file)
 
-st.title('VISUALIZATION DATA USING STREAMLIT')
-
-st.markdown("""
+st.set_page_config(page_title="NBA STATS",layout="wide")
+st.title("VISUALIZATION DATA USING STREAMLIT")
+expender_bar = st.expander("About")
+expender_bar.markdown("""
             * ***Data Source:*** [Basketball-reference.com](https://www.basketball-reference.com/)  
-            * ***Anggota Kelompok:***
+            * ***Anggota Kelompok Tugas Besar Pemrograman Fungsional:***
             1. Aldi (2110)
             2. Agyl (21102291)
             3. Reynant (2110)        
             """)
+st.markdown("""
+            ---
+            """)
 
 # sidebar year
-
-st.sidebar.header('`Filter` Here')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(1970,2020))))
+st.sidebar.header('NBA STATS')
+st.sidebar.subheader('`Filter` Here') 
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1970,2021))))  
 
 # web scraping
+# data pre processing
 
 @st.cache
 def load_data(year):
@@ -32,10 +36,9 @@ def load_data(year):
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis = 1)
     return playerstats
-playerstats = load_data(selected_year)
+playerstats = load_data(selected_year) #custom function retrieve nba player stat by selected year
 
 # sidebar team
-
 sorted_unique_team = sorted(playerstats.Tm.unique())
 selected_team = st.sidebar.multiselect('Team',sorted_unique_team,sorted_unique_team)
 
@@ -43,12 +46,22 @@ selected_team = st.sidebar.multiselect('Team',sorted_unique_team,sorted_unique_t
 unique_pos = ['C','PF','SF','PG','SG']
 selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-# Filtering data
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+#slidebar - age
+age_unique = sorted(playerstats.Age.unique())
+selected_age = st.sidebar.multiselect('Age', age_unique,age_unique)
 
-st.header('Display Player Stats of Selected Team(s)')
+# Filtering data
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos)) & (playerstats.Age.isin(selected_age))]
+df_rows = len(playerstats.axes[0])
+
+st.header(":basketball: Dashboard")
+st.markdown("""
+            **Display Plyer Stats of Selected Filter**
+            """)
 st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.write('Total of Players: ', df_rows) 
 st.dataframe(df_selected_team)
+
 
 # Download NBA player stats data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
@@ -59,7 +72,35 @@ def filedownload(df):
     return href
 
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+"""
+# visualization
+#1 bar chart
 
 
+total_game_by_team = (
+    playerstats.groupby(by=["G"]).sum()[["Tm"]].sort_values(by="Tm")
+)
+
+fig_total_game_by_team = px.bar(
+    total_game_by_team,
+    x = "Tm",
+    y = total_game_by_team.index,
+    orientation = "h",
+    title = "<b> GAME by TEAM </b>",
+    color_discrete_sequence=["#0083B8"] * len(total_game_by_team),
+    template="plotly_white",
+)
+
+fig_total_game_by_team.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False)),
+    barmode='group'
+)
+
+#BAR CHART
+if st.button('Bar Chart'):
+    st.plotly_chart(fig_total_game_by_team, theme="streamlit", use_container_width=True)
+    
+"""
 
 
